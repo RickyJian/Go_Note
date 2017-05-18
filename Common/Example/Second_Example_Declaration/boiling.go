308fa190e2a5afab14064fcea3b59b5182a79f57
RickyJian <rjricky03bc@gmail.com>
2017-05-19 01:28:27 +0800

    fixed wrong words

--- Common/Example/Second_Example_Declaration/boiling.go
+++ Common/Example/Second_Example_Declaration/boiling.go
@@ -9,5 +9,5 @@ const boilingF = 212.0 //套件層級宣告
 func main() {
 	var f = boilingF                                   // 區域變數
 	var c = (f - 32) * 5 / 9                           // 區域變數
-	fmt.Printf("boiling point = %g F or %g C\n", f, c) // %g 浮點樹輸出
+	fmt.Printf("boiling point = %g F or %g C\n", f, c) // %g 浮點數輸出
 }
